6238785dff3cd2836ab418c3fa7c36665c1c833b
Rewati Jog <Rewati.Jog@TANDON.LOCAL>
2019-07-17 12:39:37 +0530

    Add project files.

---
+++ test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("36b8fd66-affa-4265-951a-0b897e09af29")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using EAGetMail;
+
+namespace test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //string curpath = Directory.GetCurrentDirectory();
+            //string mailbox = String.Format("{0}\\inbox", curpath);
+
+            //// If the folder is not existed, create it.
+            //if (!Directory.Exists(mailbox))
+            //{
+            //    Directory.CreateDirectory(mailbox);
+            //}
+            //MailServer oServer = new MailServer("outlook.office365.com", "rewati.jog@infinx.com", "Aboli10#", ServerProtocol.Imap4);
+            ////outlook.office365.com
+            //// MailServer oServer = new MailServer("imap.gmail.com", "rewatijogn@gmail.com", "#Rewat!N07", ServerProtocol.Imap4);
+            //MailClient oClient = new MailClient("TryIt");
+
+            //// Set SSL connection
+            //oServer.SSLConnection = true;
+
+            //// Set 993 IMAP4 SSL port
+            //oServer.Port = 993;
+            //oClient.Connect(oServer);
+            
+            //MailInfo[] infos = oClient.GetMailInfos();
+            //int mcnt = oClient.GetMailCount();
+            //MailInfo info = infos[mcnt-1];
+            //Mail oMail = oClient.GetMail(info);            
+            //var msg = oMail.TextBody;
+            //string strSub = oMail.Subject;
+            //var strFrm = oMail.From;
+            //var dt = oMail.ReceivedDate;
+            //var strto = oMail.To;
+
+
+        }
+
+    }
+}
+
